38258c7f1e7fa4f3568c96b919dafd0fbebfa2af
Gustavo <o_fantasma@hotmail.com.br>
2020-03-05 19:41:51 -0300

    Starting a new project

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'login.html',
+            template: './src/login.html'
+        })
+    ]
+}
